83cb22ed052ce603c922c715ec81f52d58bf42f0
CaptainReelee <84704807+CaptaionReelee@users.noreply.github.com>
2021-08-11 17:02:22 +0700

    Midterm(Athletes)

--- exam-midterm/features/athletes-controller.js
+++ exam-midterm/features/athletes-controller.js
@@ -4,6 +4,45 @@ const athleteCreateForm = async (req, res) => {
   res.render('athlete-create-form')
 }
 
+const athleteList = async (req, res) => {
+  const query = db.collection('athletes')
+    .orderBy('name')
+    .get()
+  const athletesList = (await query).docs.map(doc => doc.data())
+
+  res.render('athletes-list', { athletesList })
+}
+const athleteDetails = async (req, res) => {
+  const slug = req.params.slug
+
+  const query = db.collection('athletes')
+    .doc(slug)
+    .get()
+  const athleteDetails = (await query).data()
+  res.render('athletes-details', { athleteDetails })
+}
+
+const athleteSchedule = async (req, res) => {
+  const slug = req.params.slug
+
+  const query = db.collection('heats')
+    .where('athleteSlugs', 'array-contains', slug)
+    .get()
+
+  const athleteSchedule = (await query).docs.map(doc => doc.data())
+  res.render('athletes-schdules', { athleteSchedule })
+}
+const athleteCreateByFrom = async (req, res) => {
+  const { slug, name, images, gender } = req.body
+  const data = { slug, name, images, gender }
+
+  const query = db.collection('athletes')
+    .doc(slug)
+    .set(data)
+
+  await query
+  res.redirect(`/athletes/${slug}`)
+}
 module.exports = {
-  athleteCreateForm,
+  athleteCreateForm, athleteList, athleteDetails, athleteSchedule, athleteCreateByFrom
 }
