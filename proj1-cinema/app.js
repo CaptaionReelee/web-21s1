423da0e533efeef702ba18c81d59c0398ae8bbd6
CaptainReelee <84704807+CaptaionReelee@users.noreply.github.com>
2021-08-25 00:08:30 +0700

    Mini-project1

--- proj1-cinema/app.js
+++ proj1-cinema/app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
+const { index } = require('./features/index-controller')
+const expHbs = require('express-handlebars')
+const { cinemaList, cinemaDetails } = require('./features/cinemaController')
+const { filmList, filmDetails } = require('./features/flimControllers')
+const { bookingForm, bookingSubmit } = require('./features/bookingController')
+const { myTickets, myTicketsAPI } = require('./features/tricketController')
 
 const app = express()
+
+// Templates
+app.set('views', './views')
+app.set('view engine', 'hbs')
+app.engine('hbs', expHbs({
+  extname: '.hbs',
+  defaultLayout: false,
+  partialsDir: ['./views/partials', './views/layouts']
+}))
 
 // Middleware
 app.use(express.static('public'))
@@ -9,6 +24,13 @@ app.use(json())
 app.use(urlencoded({ extended: false }))
 
 // Routes
-// TODO later
-
+app.get('/', index)
+app.get('/cinemas', cinemaList)
+app.get('/cinemas/:slug', cinemaDetails)
+app.get('/films', filmList)
+app.get('/films/:slug', filmDetails)
+app.get('/book', bookingForm)
+app.post('/book', bookingSubmit)
+app.get('/tickets', myTickets)
+app.get('/api/v1/tickets', myTicketsAPI)
 module.exports = { app }
