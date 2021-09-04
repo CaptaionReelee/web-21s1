97891c86af2cec2db9b8468ecb79f64acf7f8d16
CaptainReelee <84704807+CaptaionReelee@users.noreply.github.com>
2021-09-04 19:15:03 +0700

    [U8] state

--- u8-state/src/views/VideoPlaylist.vue
+++ u8-state/src/views/VideoPlaylist.vue
@@ -1,25 +1,34 @@
 <template>
-  <div class="playlist">
-    <router-link
-      v-for="video in videos"
-      :key="`playlist-${video.id}`"
-      :to="{ name: 'VideoPlayer', params: { videoId: video.id } }"
-      class="playlist-row"
-    >
-      <VideoCard :video="video" />
-    </router-link>
+  <div>
+    <h1>Queued: {{ watchLaterFull.length }} / {{ videos.length }}</h1>
+    <div class="playlist" v-if="watchLaterFull && videos">
+      <router-link
+        v-for="video in watchLaterFull"
+        :key="`playlist-${video.id}`"
+        :to="{ name: 'VideoPlayer', params: { videoId: video.id } }"
+        class="playlist-row"
+      >
+        <VideoCard :video="video" />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Video } from '@/store/models'
+import { mapGetters } from 'vuex'
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import VideoCard from './VideoCard.vue'
 
-@Component({ components: { VideoCard } })
+@Component({
+  components: { VideoCard },
+  computed: mapGetters(['watchLaterFull'])
+})
 export default class VideoPlaylist extends Vue {
-  videos: Video[] = []
+  get videos (): Video[] {
+    return this.$store.getters.watchLaterFull
+  }
 }
 </script>
 
