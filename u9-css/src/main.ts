407f9d879ce544be32e71183c9d843f1db763404
CaptainReelee <84704807+CaptaionReelee@users.noreply.github.com>
2021-09-11 17:34:47 +0700

    [U9]CSS/update2

--- u9-css/src/main.ts
+++ u9-css/src/main.ts
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import '@mdi/font/css/materialdesignicons.min.css'
+import './main.scss'
 
 Vue.config.productionTip = false
-
+Vue.use(Buefy)
 new Vue({
   router,
   store,
