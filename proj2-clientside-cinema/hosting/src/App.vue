9c28355ca718507088ddba8708ce9f6d5e742cee
CaptainReelee <84704807+CaptaionReelee@users.noreply.github.com>
2021-10-20 13:38:44 +0700

    Proj2 first commit

--- proj2-clientside-cinema/hosting/src/App.vue
+++ proj2-clientside-cinema/hosting/src/App.vue
@@ -1,9 +1,153 @@
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div
+    id="app"
+    class="has-background-light"
+  >
+    <div id="app">
+
+    <b-navbar class="is-primary">
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }" class="is-size-5">
+          <p>BECinema</p>
+        </b-navbar-item>
+        <b-navbar-item>
+            <p>Cinema</p>
+          </b-navbar-item>
+          <b-navbar-item>
+            <p>Flim</p>
+          </b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-dropdown collapsible hoverable right>
+          <template #label>
+            <b-icon icon="map-marker-radius"></b-icon>
+            <p>Phisanulok</p>
+          </template>
+          <b-navbar-item>
+            <p>Bankok</p>
+          </b-navbar-item>
+          <b-navbar-item>
+            <p>Chiang Mai</p>
+          </b-navbar-item>
+        </b-navbar-dropdown>
+
+        <b-navbar-dropdown Personal>
+          <template #label>
+            <b-icon icon="account-circle-outline"></b-icon>
+            <p>Foon</p>
+          </template>
+          <b-button class="is-primary" expanded>My Trickets</b-button>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
+    <router-view />
+  </div>
+
+    <div class="layout-footer has-text-light has-background-dark py-5">
+      <div class="container">
+        <ol>
+          <li>
+            <router-link :to="{ name: 'CinemaList' }">
+              Cinema List
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{
+                name: 'CinemaDetails',
+                params: { slug: 'phitsanulok-bec-auditorium' },
+              }"
+            >
+              Cinema Details
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'FilmList' }">
+              Film List
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{
+                name: 'FilmDetails',
+                params: { slug: 'black-pink-movie' },
+              }"
+            >
+              Film Details
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{
+                name: 'Book',
+                params: {
+                  slug: 'phitsanulok-bec-auditorium,1,1628863200',
+                },
+              }"
+            >
+              Booking Form
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'Tickets' }">
+              My Tickets
+            </router-link>
+          </li>
+        </ol>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
+
+#app {
+  min-height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+@import './main.scss';
+
+.image {
+  display: inline-block;
+  vertical-align: bottom;
+}
+
+.layout-footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.layout-footer ol {
+  margin: 0;
+  padding: 0 0 0 1rem;
+  column-count: 3;
+  column-gap: 2rem;
+}
+
+.layout-footer li {
+  padding-left: 0.5rem;
+  padding-top: 0.25rem;
+}
+
+.layout-footer ol a,
+.layout-footer ol a:hover {
+  background: inherit;
+  color: $light;
+  font-size: 1rem;
+  font-weight: bold;
+  text-decoration: underline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+</style>
